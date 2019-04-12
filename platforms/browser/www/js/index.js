e4ea69680e2438f730173fc857fbe932c52df535
Prueb-SalesF <47577289+Prueb-SalesF@users.noreply.github.com>
2019-04-12 16:31:05 -0500

    Cambia URL y boton Atras intento

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -52,7 +52,11 @@ var app = {
     }*/
 
     onDeviceReady: function() {
-        cordova.InAppBrowser.open('http://capacitaciones-developer-edition.na81.force.com/Inicio/Encuesta?id=0063600000KcCRP', '_self', 'location=yes');
+        // Register the event listener
+        document.addEventListener("backbutton", onBackKeyDown, false);
+        //URL
+        cordova.InAppBrowser.open('https://doit-lallemandinternodev001.cs77.force.com/', '_self', 'location=no', 'hideurlbar=yes', 'toolbar=no');
+        //cordova.InAppBrowser.open('http://capacitaciones-developer-edition.na81.force.com/Inicio/Encuesta?id=0063600000KcCRP', '_self', 'location=yes');
         // cordova.InAppBrowser.open('https://doitcloud-developer-edition.na34.force.com/ConsultantLoginsSignIn', '_self', 'location=yes');
         window.open = cordova.InAppBrowser.open;
         app.receivedEvent('deviceready');
