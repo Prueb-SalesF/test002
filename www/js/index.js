713c2f181a2687d51e8ee9fa68fd09ec6975512a
Prueb-SalesF <47577289+Prueb-SalesF@users.noreply.github.com>
2019-04-30 17:55:58 -0500

    Intento de usar camara 01

--- www/js/index.js
+++ www/js/index.js
@@ -55,7 +55,7 @@ var app = {
 
     onDeviceReady: function() {        
         //URL
-        cordova.InAppBrowser.open('https://doit-lallemandinternodev001.cs77.force.com/', '_self', 'location=no', 'hideurlbar=yes', 'toolbar=no');
+        cordova.InAppBrowser.open('https://doit-lallemandinternodev001.cs77.force.com/', '_blank', 'location=no', 'hideurlbar=yes', 'toolbar=no');
         //cordova.InAppBrowser.open('https://seguimientointerno-developer-edition.na85.force.com/InternalLogin/', '_self', 'location=no', 'hideurlbar=yes', 'toolbar=no');
         // cordova.InAppBrowser.open('https://doitcloud-developer-edition.na34.force.com/ConsultantLoginsSignIn', '_self', 'location=yes');
         window.open = cordova.InAppBrowser.open;
